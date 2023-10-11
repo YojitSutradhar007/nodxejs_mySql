79d4f73b2aced11f6af14a3434cb809aa7f26266
SutradharYojit <jimmysuthar08@gmail.com>
2023-10-11 16:11:38 +0530

    minor change

--- routes/send_mail.js
+++ routes/send_mail.js
@@ -19,7 +19,21 @@ router.get('/sendEmail', async (req, res, next) => {
         to: "jimmysuthar789@gmail.com", // list of receivers
         subject: "Hello Jimmy Suthar", // Subject line
         text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        html: `<html>
+        <head>
+            <title>Beautiful HTML with Hyperlinks</title>
+        </head>
+        <body>
+            <h1>Welcome to My Beautiful HTML Page</h1>
+            <p>This is a sample HTML page with hyperlinks. Below are some links to various websites:</p>
+            <ul>
+                <li><a href="https://www.example.com">Visit Example.com</a></li>
+                <li><a href="https://www.google.com">Go to Google</a></li>
+                <li><a href="https://www.github.com">Check out GitHub</a></li>
+            </ul>
+            <p>Feel free to explore these links and create your own beautiful HTML pages!</p>
+        </body>
+        </html>` // html body
     }).then((method) => {
         console.log("Message sent: %s", method.messageId);
         return res.status(200).json({
